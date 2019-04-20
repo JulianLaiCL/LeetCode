edde17cbb0c497930459b553b48758e91147279e
julian.lai <chunlun.lai@gmail.com>
2019-04-20 23:18:36 +0800

    45.cpp. reference solution from others; O(N); O(1)

--- 45.cpp
+++ 45.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cassert>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -9,37 +10,26 @@ class Solution
 public:
     int jump(vector<int>& nums) 
     {
-        minstep = nums.size();
-        if (minstep <= 1) return 0;
+        if (nums.size() <= 1) return 0;
         if (nums[0] == 0) return 0;
-        if (nums[0] >= (minstep-1)) return 1;
-        
-        dfs(nums, nums.size()-1, 0);
-        cout<<minstep<<endl;
-        return minstep;
-    }
-private:
-    int minstep = 0;
-    void dfs(vector<int>& nums, int pos, int count)
-    {
-        ++count;
-        if ((count) >= minstep) return;
-        else if (pos <= nums[0])
+        if (nums[0] >= (nums.size())) return 1;
+
+        int start = 0, end = 0, max = 0, step = 0;
+        while (end < (nums.size()-1))
         {
-            minstep = count;
-            return;
+            ++step;
+            for (int i = start; i < (end+1); ++i)
+                max = std::max(max, i+nums[i]);
+                
+            start = end + 1;
+            end = max;
         }
-        
-        for (int i = pos-1; i >= 1; --i)
-        {
-            if (nums[i] <= 0) continue; //ignore zero, check next one
-            if (nums[i] < (pos-i)) continue; //unreachable, check next one
-            dfs(nums, i, count);
-        }
+        //cout<<step<<endl;
+        return step;
     }
 };
 
-#if 1
+#if 0
 int main()
 {
     Solution* sol = new Solution();
@@ -73,7 +63,7 @@ int main()
     nums = {3,4,6,3,0,1,2,1,1,1};
     assert(sol->jump(nums) == 3);
     nums = {8,2,4,4,4,9,5,2,5,8,8,0,8,6,9,1,1,6,3,5,1,2,6,6,0,4,8,6,0,3,2,8,7,6,5,1,7,0,3,4,8,3,5,9,0,4,0,1,0,5,9,2,0,7,0,2,1,0,8,2,5,1,2,3,9,7,4,7,0,0,1,8,5,6,7,5,1,9,9,3,5,0,7,5};
-    sol->jump(nums);
+    assert(sol->jump(nums) == 13);
     
     free(sol);
 }
