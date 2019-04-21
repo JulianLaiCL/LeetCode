a1cc3b341b1cf73ed4606cbc42928ed8d842e86c
julian.lai <chunlun.lai@gmail.com>
2019-04-21 13:14:02 +0800

    46.cpp; O(N!); O(N!*N)

---
+++ 46.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution 
+{
+public:
+    vector<vector<int>> permute(vector<int>& nums) 
+    {
+        len = nums.size();
+        if (len == 0) return {};
+        if (len == 1) return {{nums[0]}};
+
+        ans.clear();
+        recpermute(nums, 0);
+        return ans;
+    }
+    void dump(vector<vector<int>>& ans)
+    {
+        cout<<"sizeof ans is "<<ans.size()<<", dump ans: "<<endl;
+        for (int i = 0; i < ans.size(); ++i)
+        {
+            for (int k = 0; k < ans[i].size(); ++k)
+                cout<<ans[i][k]<<" ";
+            cout<<endl;
+        }
+    }
+private:
+    vector<vector<int>> ans;
+    int len;
+    void recpermute(vector<int>& nums, int pos)
+    {
+        int swapidx = 0;
+        
+        if (pos == len-1)
+        {
+            ans.push_back(nums);
+            return;
+        }
+        
+        for (int i = pos; i < len; ++i)
+        {
+            std::swap(nums[i], nums[pos]);
+            recpermute(nums, pos+1);
+            std::swap(nums[pos], nums[i]);
+        }
+    }
+};
+
+#if 0
+int main()
+{
+    Solution* sol = new Solution();
+    vector<int> nums;
+    vector<vector<int>> ans;
+
+    ans = sol->permute(nums);
+    sol->dump(ans);
+    nums = {0};
+    ans = sol->permute(nums);
+    sol->dump(ans);
+    nums = {1};
+    ans = sol->permute(nums);
+    sol->dump(ans);
+    nums = {1,2,3};
+    ans = sol->permute(nums);
+    sol->dump(ans);
+    nums = {4,5,6,1};
+    ans = sol->permute(nums);
+    sol->dump(ans);
+    nums = {99,4,5,6,1};
+    ans = sol->permute(nums);
+    sol->dump(ans);
+
+    free(sol);
+}
+#endif
